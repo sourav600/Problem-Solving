126d90252a2dbabb6400be8a474db0fb4a2d6603
Moinul Islam Sourav <68951478+sourav600@users.noreply.github.com>
2022-02-18 22:05:29 +0600

    10324- Zeros and ones

--- UVa/494 - Kindergarten Counting Game/main.cpp
+++ UVa/494 - Kindergarten Counting Game/main.cpp
@@ -11,10 +11,15 @@ int main(){
     string str;
     while(getline(cin,str)){
         int count = 0;
-        istringstream iss(str);
-        string word;
-        while(iss>>word){
-            if((word[0]>='a' && word[0]<='z') || (word[0]>='A' && word[0]<='Z')) count++;
+        int temp = 0;
+        for (int i=0; str[i]!='\0'; i++){
+             if ((str[i]>=65 && str[i]<=90 || str[i]>=97 && str[i]<=122)){
+                if(temp==0) {
+                    count++;
+                    temp = 1;
+                }
+             }
+             else temp = 0;
         }
         cout<<count<<endl;
     }
