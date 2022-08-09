df994b3593e0f59de14c91b26933553aa78c8dbd
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-08-09 21:21:18 +0600

    Efficient Pseudo Code

--- LightOJ/EfficientPseudoCode.cpp
+++ LightOJ/EfficientPseudoCode.cpp
@@ -2,41 +2,91 @@
 using namespace std;
 
 #define nl '\n'
+typedef long long ll;
 #define For(i,n) for(int i=0; i<n; i++)
 #define vi vector<int>
 #define vs vector<string>
 #define vl vector<ll>
 #define pb(x) push_back(x)
 #define pi acos(-1)
-typedef long long ll;
 typedef unsigned long ulong;
+const ll M = 1000000007;
+vector<ll> v;
+unordered_map<ll,ll> um;
+unordered_map<ll,ll>::iterator it;
+ll mod(ll a){
+    return ((a%M)+M)%M;
+}
 
-ll power(int n, int m){ 
+ll power(ll n, ll m){ 
     if(m==0) return 1; //base condition
-     
-    ll temp = power(n,m/2);
-    if(m&1) return n * temp * temp;
-    else return temp*temp;
+    ll temp = mod(power(n,m/2));
+    if(m&1) return mod(mod(n) * mod(temp*temp));
+    else return mod(temp*temp);
+}
+void primeFactor(ll n){
+    for(ll j=2; j*j<=n; ++j){
+        if(n%j==0){
+            v.pb(j);
+            while(n%j==0){
+                um[j]++;
+                n/=j;
+            }
+        }
+    }
+    if(n>1){
+        if(um[n]==0) v.pb(n);
+        um[n]++;
+    }
 }
 int main(){
     #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
     #endif
-    cout<<pow(2,30)<<nl;
-    int t,n,m; cin>>t;
+    ll t,n,m; cin>>t;
     For(i,t){
+        v.clear(); um.clear();
         cin>>n>>m;
-        ll p = power(n,m);
-        //cout<<p<<nl;
-        int sum=0;
-        for(int j=1; j*j<=p; j++ ){
-            if(p%j==0) {
-                if(j*j == p) sum+=j;
-                else sum+= j+(p/j);
-            }
+        primeFactor(n);
+        for(auto p:v){
+            um[p]*=m;
         }
-        cout<<"Case "<<i+1<<": "<<sum<<nl;
+        //for(it=um.begin();it!=um.end();++it)cout<<it->first<<" "<<it->second<<nl;
+        ll ans=1;
+        for(auto p:v){
+            ll numeretor = power(p,um[p]+1) - 1;
+            ll denominator = power(p-1,M-2);
+            ans = mod(mod(ans)*mod(numeretor*denominator));
+        }
+        cout<<"Case "<<i+1<<": "<<ans<<endl;
     }
+return 0;
+}
+#include<bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define um unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+typedef long long ll;
+typedef unsigned long ulong;
+const ll M = 1000000007;
+
+ll mod(ll a){
+    return ((a%M)+M)%M;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+    #endif
+
 return 0;
 }
