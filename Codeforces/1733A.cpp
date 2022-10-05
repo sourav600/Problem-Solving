c4143af8c0c72c48724a15948dfc67e17a51b832
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-10-05 12:11:15 +0600

    1733A - Consecutive Sum

---
+++ Codeforces/1733A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    ll n,k,sum=0; cin>>n>>k;
+    vi v(n);
+    For(i,n)cin>>v[i];
+    For(i,k){
+        int mx = 0;
+        for(int j=i; j<n;j+=k) mx = max(mx,v[j]);
+        sum+=mx;
+    }
+    cout<<sum<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
