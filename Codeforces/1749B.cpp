91df2d74b270e5a8abad8c374df5e0f58bc3d7b9
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-10-20 21:48:01 +0600

    1749B.cpp

---
+++ Codeforces/1749B.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    ll n,ans=0,x; cin>>n;
+    vl h(n),s(n);
+    
+    For(i,n) {
+        cin>>h[i];
+        ans += h[i];
+    }
+    For(i,n){
+        cin>>s[i];
+        ans+=s[i];
+    }
+    ans -= *max_element(s.begin(),s.end());
+    cout<<ans<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
