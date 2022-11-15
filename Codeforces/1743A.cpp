c98d3ccec68665c7ec13637449eb2ee006443163
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-11-15 23:16:01 +0600

    1743A

---
+++ Codeforces/1743A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    int n,x; cin>>n;
+    For(i,n) cin>>x;
+    x = 10-n;
+    int ans = (x*(x-1))/2;
+    cout<<ans*6<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
