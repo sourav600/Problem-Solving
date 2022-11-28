1756f9ae336ba77b11ea5f0943676a38e32fe09c
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-11-28 09:07:57 +0600

    A vvvwww

---
+++ AtCoder/A_wwwvvvvvv.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    string s;
+    cin>>s;
+    int ans=0;
+    For(i,s.size()){
+        if(s[i]=='v') ++ans;
+        else ans += 2;
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1;
+
+    solve(tc++);
+
+return 0;
+}
