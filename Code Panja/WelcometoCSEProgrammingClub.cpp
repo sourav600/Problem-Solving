65e0b153442372ddc2d2302b9d3bada4332cf32f
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-12-01 21:57:37 +0600

    Welcome to CSE

---
+++ Code Panja/WelcometoCSEProgrammingClub.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(){
+    cout<<"PUST-CSE Intra Department Programming Contest. Organize By CSE Department Programming Club & Sponsored By CodePanja\n";
+}
+
+int main(){
+
+    solve();
+
+return 0;
+}
