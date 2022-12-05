750cb54a710b40e8a41e5c76e3f9b6583bcc3220
Moinul Islam Sourav <mrsourav600@gmail.com>
2022-12-05 16:07:24 +0600

    Max Heap

--- Assignment/LinedList.cpp
+++ Assignment/LinedList.cpp
@@ -2,16 +2,6 @@
 using namespace std;
 #define nl '\n'
 #define For(i,n) for(int i=0; i<n; i++)
-#define vi vector<int>
-#define vs vector<string>
-#define vl vector<ll>
-#define mi map<int,int>
-#define umi unordered_map<int,int>
-#define pb(x) push_back(x)
-#define pi acos(-1)
-typedef long long ll;
-typedef unsigned long ulong;
-const ll M = 1000000007;
 #define SIZE 5
 int total;
 
