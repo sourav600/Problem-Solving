029b4532d58fd7ebfc67ed6abfcfb82a4959d3b0
Moinul Islam Sourav <moinulmd306@gmail.com>
2022-12-29 09:59:33 +0600

    1731B

---
+++ Codeforces/1731B.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1e9+7;
+
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void solve(int tc){
+    //ll n; cin>>n;
+    //ll ans = ((((n*(n+1))%M)*(4*n-1))%M*337)%M;
+    __int128 n,sumOfDiagonal,sumOfOther,ans;
+    n=read();
+    sumOfDiagonal = n*(n+1)*(2*n+1)/6;
+    --n;
+    sumOfOther = n*(n+1)*(2*n+4)/6;
+    ans = ((sumOfDiagonal + sumOfOther)*2022)%M;
+    print(ans);
+    cout<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
