3abca8cc2242aafeb068321d887a188faaf825ed
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-02-10 15:42:09 +0600

    Mock test A

---
+++ CodeMarshel/MocA.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int sum=a+b+c;
+    printf("Sum of %d, %d and %d is %d\n",a,b,c,sum);
+}
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+
+    
+    solve();
+
+return 0;
+}
