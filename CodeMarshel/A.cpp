a6c0a9d662d8d8992f453d001404b13e43016319
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-02-11 23:48:27 +0600

    ICPC2 A

---
+++ CodeMarshel/A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int count = 0;
+        string s1,s2,s3;
+        cin>>s1>>s2>>s3;   
+        for(int i=0; i<n;++i){
+            if(s1[i]=='O' && s1[i+1]=='O' && s2[i]=='O' && s2[i+1]=='O') {
+                count++;i++; continue;
+            }
+            else if(s2[i]=='O' && s2[i+1]=='O' && s3[i]=='O' && s3[i+1]=='O'){
+                count++; i++;
+            }
+        }
+        cout<<count<<nl;
+    }
+    
+
+return 0;
+}
