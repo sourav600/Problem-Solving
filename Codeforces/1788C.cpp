f20bf63b2461cff3b5588feb9a802d7ece969c09
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-02-13 11:49:18 +0600

    1788C

---
+++ Codeforces/1788C.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    int n; cin>>n;
+    int x=n*2 + 1;
+    if(n%2){
+        cout<<"Yes\n";
+        
+    }
+    else cout<<"No\n";
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
