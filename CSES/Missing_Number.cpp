4e3b9a66de00e4007335a360f6f06b6b867e1fb9
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-02-19 10:52:42 +0600

    missing numbers

---
+++ CSES/Missing_Number.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=1; i<=n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+    int n,x; cin>>n;
+    vi v(n+1,0);
+    For(i,n-1){
+        cin>>x;
+        v[x] = x;
+    }
+    For(i,n)
+        if(v[i]==0) cout<<i<<nl;
+    
+return 0;
+}
