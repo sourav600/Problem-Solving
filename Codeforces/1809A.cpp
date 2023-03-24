cd6bb9ba3c0d7f8508f608bd0d032edb81e68ea1
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-03-24 10:12:52 +0600

    1809A

---
+++ Codeforces/1809A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    string s;
+    cin>>s;
+    unordered_map<char,int> um;
+    For(i,s.size()) um[s[i]]++;
+    if(um.size()==1) cout<<-1<<nl;
+    else if(um.size()==2 && um[s[0]]!=2) cout<<6<<nl;
+    else cout<<4<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
