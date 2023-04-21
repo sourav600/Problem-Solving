4963ebd61e0887fc12674abaa6ebcd609bc2b3e0
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-04-21 06:01:37 +0600

    147A

---
+++ Codeforces/147A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll M = 1000000007;
+
+void solve(int tc){
+    string s; cin>>s;
+    if(s[0]=='0'){
+        cout<<0<<nl;
+        return;
+    }
+    int ans=1;
+    For(i,s.size()){
+        if(i==0 && s[i]=='?') ans=9;
+        else if(s[i]=='?') ans*=10;
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
