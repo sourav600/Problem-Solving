3e52802c6176c3de919fccbd4c1c71721cbd1a39
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-05-12 12:28:34 +0600

    two gangstar

---
+++ VJudge/TwoGangsters.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=0; i<n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define vpp vector<pair<int,int> >
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll INF = 1e9 + 7;
+const ll N = 1e5 + 10;
+
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int n,m;
+    cin>>n>>m;
+    cout<<m-1<<" "<<n-1<<nl;
+    
+    
+    
+    
+
+return 0;
+}
