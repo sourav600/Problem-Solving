210fccd4c2dfc2b434fbf543faaa667f6333148d
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-05-14 09:26:57 +0600

    knights in chess

---
+++ VJudge/KnightsInChessboard.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=1; i<=n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define vpp vector<pair<int,int> >
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll INF = 1e9 + 7;
+const ll N = 1e5 + 10;
+
+void solve(int tc){
+    cout<<"Case "<<tc<<": ";
+    int n,m; cin>>n>>m;
+    if(n==1 || m==1 ) cout<<max(n,m)<<nl;
+    else {
+        cout<<ceil((n*m*1.0)/2.0)<<nl;
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
