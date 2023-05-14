bed4bd185761aaa87c31f6557acb4f43c6584745
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-05-14 09:26:44 +0600

    secret origin

---
+++ VJudge/SecretOrigin.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=1; i<=n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define vpp vector<pair<int,int> >
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll INF = 1e9 + 7;
+const ll N = 1e5 + 10;
+
+void solve(int tc){
+    int n; cin>>n;
+    bitset<32> b=n;
+    string s = b.to_string();
+    cout<<s<<nl;
+    next_permutation(s.begin(),s.end());
+    cout<<s<<nl;
+    unsigned long decimal = bitset<32>(s).to_ulong();
+    cout<<desimal<<nl;
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
