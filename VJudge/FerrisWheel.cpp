fde6c164beebf7a08cd9a3b50f9faf090e292f9e
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-05-16 21:02:35 +0600

    Ferris wheel

---
+++ VJudge/FerrisWheel.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long ulong;
+#define nl '\n'
+#define For(i,n) for(int i=1; i<=n; i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vl vector<ll>
+#define mi map<int,int>
+#define umi unordered_map<int,int>
+#define vpp vector<pair<int,int> >
+#define pb(x) push_back(x)
+#define pi acos(-1)
+const ll INF = 1e9 + 7;
+const ll N = 1e5 + 10;
+
+
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n,m;
+    cin>>n>>m;
+    vi v(n+1);
+    For(i,n) cin>>v[i];
+    sort(v.begin(),v.end());
+    int l=1, r=n,ct=0;
+    while(l<=r){
+        if(v[l]+v[r]<=m) {
+            ++l;
+            --r;
+        }
+        else{
+            --r;
+        }
+        ++ct;
+    }
+    cout<<ct<<nl;
+
+return 0;
+}
