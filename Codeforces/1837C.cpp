b9ec4d2cddef8d67ca60935d805fc63753b28f73
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-05-25 22:14:56 +0600

    1837C

---
+++ Codeforces/1837C.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define For(i,a,n) for(int i=a; i<=n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 1e5 + 10;
+
+void solve(int tc){
+	string s;
+	cin>>s;
+	int n = s.size();
+	if(s[0]=='?') s[0] = '0';
+	for(int i=1; i<n; ++i){
+		if(s[i]=='?'){
+			if(s[i-1] == '1') s[i] = '1';
+			else s[i] = '0';
+		}
+	}
+	cout<<s<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
