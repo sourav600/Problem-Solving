e4ebd32a961566c3f8da29b1a205a5ce74af9037
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-06-07 15:30:28 +0600

    1840A

---
+++ Codeforces/1840A.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define For(i,n) for(int i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+
+void solve(int tc){
+	int n; cin>>n;
+	cin.ignore();
+	string s;
+	cin>>s;
+	vector<char> v;
+	For(i,n){
+		//cout<<i<<nl;
+		char cur = s[i];
+		v.push_back(cur);
+		++i;
+		while(i<n && s[i]!=cur) ++i;
+		//cout<<i<<nl;
+	}
+	For(i,v.size()) cout<<v[i];
+	cout<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
