491d693f8380f22170d65fac10a28ddfa09d8964
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-06-07 15:30:14 +0600

    1520F1

---
+++ VJudge/GuesstheK_thZero.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define For(i,n) for(int i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+
+    int n,t,k;
+    cin>>n>>t>>k;
+    int lo=1, hi=n, ct=0;
+    int one,ans=-1;
+    while(lo<=hi){
+    	int mid = lo+(hi-lo)/2;
+    	cout << "? " << lo << " " << mid << '\n';
+        cout.flush();
+        cin >> one;
+        int zero = mid-lo+1-one;
+    	ct += zero;
+
+    	if(ct<k){
+    		lo = mid+1;
+    	}else{
+    		ans = mid;
+    		ct -= zero;
+    		hi = mid-1;
+    	}
+    }
+    cout<<"! "<<ans<<nl;
+
+
+return 0;
+}
+
+
+
