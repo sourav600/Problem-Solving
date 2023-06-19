4d5f842019910527b3edc38297dd6bcf75453488
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-06-19 12:02:02 +0600

    1834B

---
+++ Codeforces/1836B.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define For(i,n) for(int i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+
+void solve(int tc){
+	ll n,k,g;	cin>>n>>k>>g;
+
+	ll ans1 = k*g, ans2 = ((g+1)/2)-1;
+	if(ans1 - ans2 * n < 0) {
+		cout<<ans1<<nl; 
+		return;
+	}
+	else{
+		cout<<ans1 - ((((ans1-ans2*n+g-1)/g)*g))<<nl;
+	}
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
+
+
+
