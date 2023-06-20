7bbdf5da776f64e05aed5573171378474e2e0112
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-06-20 22:16:34 +0600

    building roads

---
+++ VJudge/BuildingRoads.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define For(i,n) for(int i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+
+vector<int> adj[N];
+bool vis[N];
+
+void dfs(int ver){
+	vis[ver] = true;
+	for(auto child : adj[ver]){
+		if(vis[child]) continue;
+		dfs(child);
+	}
+}
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n,m;
+    cin>>n>>m;
+    int u,v;
+    For(i,m){
+    	cin>>u>>v;
+    	adj[u].push_back(v);
+    	adj[v].push_back(u);
+    }
+    vector<int> ans;
+    for(int i=1; i<=n; ++i){
+    	if(vis[i]) continue;
+    	ans.push_back(i);
+    	dfs(i);
+    }
+    cout<<ans.size()-1<<nl;
+    for(int i=0; i<ans.size()-1; ++i) cout<<ans[i]<<" "<<ans[i+1]<<nl;
+
+return 0;
+}
+
+
+
