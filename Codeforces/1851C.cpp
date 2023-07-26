8461200f92aae32874e2fc686f2a9a0e0f29eaae
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-07-26 21:30:54 +0600

    1851C

---
+++ Codeforces/1851C.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define For(i,n) for(int i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+
+typedef long long ll;
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+
+void solve(int tc){
+	int n,k;
+	cin>>n>>k;
+	vi v(n);
+	unordered_map<int,int> um;
+	For(i,n) {
+		cin>>v[i];
+	}
+	int i=1,ct1=1,ct2=1;
+	while(i<n){
+		if(v[i]==v[0]){
+			++ct1;
+		}
+		if(ct1==k) break;
+
+		++i;
+	}
+	while(i<n-1){
+		if(v[i]==v[n-1]){
+			++ct2;
+		}
+		++i;
+	}
+
+	if(v[0]==v[n-1]){
+		if(ct1>=k){
+			cout<<"YES\n";
+			return;
+		}
+	}
+	if(ct1>=k && ct2>=k){
+		cout<<"YES\n";
+	}else{
+		cout<<"NO\n";
+	}
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    solve(tc++);
+
+return 0;
+}
+
+
+
