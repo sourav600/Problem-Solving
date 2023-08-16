6229975789770049475cc7071dbd50a751b43163
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-08-17 00:29:19 +0600

    tz

---
+++ VJudge/TrailingZeros.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 1000010;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+bool mark[N];
+vl primes;
+void seive(){
+	for(ll i=3; i*i<=N; i+=2){
+		if(mark[i]==0){
+			for(ll j=i*i; j<N; j += (i+i)){
+				mark[j] = 1;
+			}
+		}
+	}
+
+	primes.push_back(2);
+	for(ll i=3; i<N; i+=2){
+		if(mark[i]==0) primes.push_back(i);
+	}
+}
+
+
+void SOURAV(int tc){
+	//for(int i=0;i<10;++i) cout<<primes[i]<<" ";
+	ll n,ans=1; cin>>n;
+
+	for(ll i=0; i<N && primes[i]*primes[i]<=n; ++i){
+		if(n%primes[i]==0){
+			ll ct=1;
+			while(n%primes[i]==0){
+			    n/=primes[i];
+				++ct;
+			}
+			ans *= ct;
+		}
+	}
+	if(n!=1){
+		ans *= 2;
+	}
+	cout<<"Case "<<tc<<": "<<ans-1<<nl;
+}
+
+int main(){
+	// #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    seive();
+    int t,tc=1; cin>>t;
+    while(t--)
+    SOURAV(tc++);
+
+return 0;
+}
+
+
+
