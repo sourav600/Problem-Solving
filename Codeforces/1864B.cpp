1e45237288fc3fe7beed9c6f41b6f1ee9c1919e4
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-08-29 07:55:53 +0600

    1864B

---
+++ Codeforces/1864B.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+	ll n,k;
+	cin>>n>>k;
+	string s;
+	cin>>s;
+
+	if(k%2==0){
+		sort(all(s));
+		cout<<s<<nl;
+	}else{
+		string even = "", odd="";
+		For(i,n){
+			if(i%2) odd.push_back(s[i]);
+			else even.push_back(s[i]);
+		}
+		sort(all(even));
+		sort(all(odd));
+		int i=0, j=0;
+		//cout<<odd<<" "<<even<<nl;
+		For(k,n){
+			if(k&1) cout<<odd[i++];
+			else cout<<even[j++];
+		}cout<<nl;
+	}
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    SOURAV(tc++);
+
+return 0;
+}
+
+
+
