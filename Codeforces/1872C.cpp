569c85bfb6c933e845028b8cdcd8b93a29487a0e
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-09-12 15:56:18 +0600

    1872C

---
+++ Codeforces/1872C.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+bool isPrime(ll n){
+    for(int i=2; i*i<=n; ++i){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+
+void SOURAV(int tc){
+    ll l,r;  cin>>l>>r;
+
+    if(r<=3) cout<<-1<<nl;
+    else if(l==r && isPrime(l)) cout<<-1<<nl;
+    else if(l==r){
+        if(l&1){
+            ll val = -1;
+            for(ll i=3; i<=r; ++i){
+                if(l%i==0){
+                    val = i;
+                    break;
+                }
+            }
+            cout<<val<<" "<<l-val<<nl;
+        }else{
+            cout<<2<<" "<<l-2<<nl;
+        }
+    }
+    else{
+        cout<<2<<" ";
+        r%2==0 ? cout<<r-2<<nl : cout<<r-3<<nl;
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    SOURAV(tc++);
+
+return 0;
+}
+
+
+
