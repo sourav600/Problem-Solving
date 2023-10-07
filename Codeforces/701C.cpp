eb407f3a99804a1afe188c69fcab258c48214819
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-10-07 17:44:17 +0600

    701

---
+++ Codeforces/701C.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(){
+    ll n; cin>>n;
+    cin.ignore();
+    string s; cin>>s;
+
+    unordered_map<char, int> um;
+
+    For(i,n) um[s[i]]++;
+
+    ll l=0, r=n-1;
+    while(l<=r){
+        if(um[s[l]]==1 && um[s[r]]==r) break;
+        if(um[s[l]]>1) ++l;
+        if(um[s[r]]>1) --r;
+    }
+    cout<<r-l+1<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
+    SOURAV();
+
+return 0;
+}
+
+
+
