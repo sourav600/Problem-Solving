36ce0e10024d927dabccfc31036469388317799a
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-12-28 23:21:31 +0600

    1915A,B

---
+++ Codeforces/1915B.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+    int a=0, b=0, c=0;
+    char arr[3][3];
+    For(i,3){
+        For(j,3){
+            cin>>arr[i][j];
+            if(arr[i][j]=='A')++a;
+            if(arr[i][j]=='B')++b;
+            if(arr[i][j]=='C')++c;
+        }
+    }
+    if(a==2) cout<<"A\n";
+    if(b==2) cout<<"B\n";
+    if(c==2) cout<<"C\n";
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    SOURAV(tc++);
+
+return 0;
+}
+
+
+
