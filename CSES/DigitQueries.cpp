71cef78c8c39c9be6e2d9d968d3aab7fd270e983
Moinul Islam Sourav <moinulmd306@gmail.com>
2024-01-01 23:40:54 +0600

    cses digit queries solution

---
+++ CSES/DigitQueries.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    ll q,k; cin>>q;
+    while(q--){
+        cin>>k;
+        ll digit = 1, n=k, temp = 9;
+        while(temp*digit < n){
+            n -= temp*digit;
+            ++digit;
+            temp *= 10;
+        }
+        //cout<<n<<nl;
+        --n;
+        ll start =1;
+        For(i,digit-1) start *= 10;
+        ll pos = n/digit;
+        ll num = start+pos;
+
+        ll p = n%digit;
+        string s = to_string(num);
+        //cout<<n<<" "<<s<<" "<<s[digit-p-1]<<nl;
+        //if(p==0) cout<<s[digit-1]<<nl;
+        cout<<s[p]<<nl;
+
+    }
+
+return 0;
+}
+
+
+
