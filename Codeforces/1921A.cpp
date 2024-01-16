2dd3b7e9aeecde3e0277736f4ad07c56f70fa39d
Moinul Islam Sourav <moinulmd306@gmail.com>
2024-01-16 10:08:02 +0600

    1921A,B

---
+++ Codeforces/1921A.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+    // int x1,y1, x2,y2,x3,y3,x4,y4;
+    // cin>>x1>>y1>>x2>>y2>>x3>>y3>>x4>>y4;
+    vpi v(4);
+    For(i,4){
+        cin>>v[i].first>>v[i].second;
+    }
+    for(int i=1; i<4; ++i){
+        if(v[0].first==v[i].first){
+            int a = abs(v[i].second - v[0].second);
+            cout<<a*a<<nl;
+        }
+    }
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    SOURAV(tc++);
+
+return 0;
+}
+
+
+
