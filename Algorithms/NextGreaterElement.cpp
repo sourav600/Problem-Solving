969ed67b6915a2536f740be35572520d6bf7a390
Moinul Islam Sourav <moinulmd306@gmail.com>
2024-01-18 13:08:55 +0600

    Next greater element

---
+++ Algorithms/NextGreaterElement.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void NGE(vl v, int n){
+    vl nge(n);
+    stack<ll> st;
+    For(i,n){
+        while(!st.empty() && v[i] > v[st.top()]){
+            nge[st.top()] = i;
+            st.pop();
+        }
+        st.push(i);
+    }
+    while(!st.empty()) {
+        nge[st.top()] = -1;
+        st.pop();
+    }
+    For(i,n) cout<<(nge[i]==-1 ? -1 : v[nge[i]])<<" ";
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n; cin>>n;
+    vl v(n);
+    For(i,n) cin>>v[i];
+    NGE(v,n);
+
+return 0;
+}
+
+
+
