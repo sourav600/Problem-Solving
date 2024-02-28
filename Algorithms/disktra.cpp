792d68adb75e4a2218b2a6ef248fbe29ec6d8ee7
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-02-28 21:00:10 +0600

    Dijkstra

--- Algorithms/disktra.cpp
+++ Algorithms/disktra.cpp
@@ -6,6 +6,7 @@ const int N = 1e5+10;
 const int INF = 1e9+10;
 
 vector<pair<ll, ll> > g[N];
+  ll v,e;
 
 void dijkstra(ll src){
     vector<ll> dist(N, INF);
@@ -32,15 +33,24 @@ void dijkstra(ll src){
             }
         }
     }
+    for(int i=1;i<=v;++i) cout<<dist[i]<<" ";
 }
 
 int main(){
-    ll v,e;
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
     cin>>v>>e;
     for(ll i=0; i<e; ++i){
         ll x, y, wt;
         cin>>x>>y>>wt;
         g[x].push_back({y,wt});
     }
+    dijkstra(1);
+
     return 0;
 }
