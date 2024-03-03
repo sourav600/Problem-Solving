3a61efcdc76a707ddcbf16793e8e0b3d249692bf
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-03-03 12:12:08 +0600

    Playlist in CSES

---
+++ CSES/Playlist.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(int i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+
+}
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n; cin>>n;
+    vi v(n);
+    For(i,n) cin>>v[i];
+    unordered_map<int,int>um;
+    um.reserve(4096);
+    um.max_load_factor(0.25);
+    int  mx=1, j=0;
+    For(i,n){
+       j = max(j,um[v[i]]);
+       mx = max(mx, i-j+1);
+       um[v[i]] =  i+1;
+    }
+    cout<<mx<<nl;
+
+return 0;
+}
+
+
+
