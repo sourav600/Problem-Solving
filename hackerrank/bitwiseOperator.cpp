db907eac5de920046133e036be8b681bfbc4396e
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-05-26 14:49:27 +0600

    Bitwise operations

---
+++ hackerrank/bitwiseOperator.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+
+    int n,k;
+    cin>>n>>k;
+    int a=0,b=0,c=0;
+    for(int i=1; i<n; ++i){
+        for(int j=i+1; j<=n; ++j){
+            //cout<<(a&j)<<" "<<(i|j)<<" "<<(i^j)<<nl;
+            if((i&j) < k) a = max(a, i&j);
+            if((i|j) < k) b = max(b, i|j);
+            if((i^j) < k) c = max(c, i^j);
+        }
+    }
+    cout<<a<<nl<<b<<nl<<c<<nl;
+
+return 0;
+}
