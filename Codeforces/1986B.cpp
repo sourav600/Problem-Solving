ead5df04f485daf7174a10a96fb2855ddd4f79e3
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-06-24 08:12:14 +0600

    1986B

---
+++ Codeforces/1986B.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+int n,m;
+bool isValid(int x, int y){
+    return x>=0 && y>=0 && x<n && y<m;
+}
+void SOURAV(int tc){
+     cin>>n>>m;
+    int arr[n][m];
+    For(i,n){
+        For(j,m) cin>>arr[i][j];
+    }
+
+    for(int i=0; i<n; ++i){
+        for(int j=0;j<m; ++j){
+            int mx = INT_MIN;
+            for(auto movement: movements){
+                int dx = i + movement.first;
+                int dy = j + movement.second;
+                if(isValid(dx,dy)){
+                    mx = max(mx, arr[dx][dy]);
+                }
+            }
+            arr[i][j] = min(mx, arr[i][j]);
+
+
+        }
+    }
+    For(i,n){
+        For(j,m) cout<<arr[i][j]<<" ";
+        cout<<nl;
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int t,tc=1; cin>>t;
+    while(t--)
+    SOURAV(tc++);
+
+return 0;
+}
+
+
+
