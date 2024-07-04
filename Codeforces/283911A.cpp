9d6f0abc829e7f326b08041a55d9f57d0ab4ef85
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-07-04 20:02:01 +0600

    Binary search (step 1)

---
+++ Codeforces/283911A.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+    ll n,q; cin>>n;
+    vl v(n);
+    For(i,n) cin>>v[i];
+    sort(all(v));
+    // for(auto it: v) cout<<it<<" ";
+    //     cout<<nl;
+
+    cin>>q;
+    ll l,r;
+    while(q--){
+        cin>>l>>r;
+        ll lo=0, hi=n-1;
+        ll greaterThan_r_count = 0;
+        while(lo<=hi){
+            ll mid = lo + (hi-lo)/2;
+
+            if(v[mid] > r) {
+                greaterThan_r_count = n-mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        // cout<<greaterThan_r_count<<" ";
+
+        lo=0, hi=n-1;
+        ll lessThan_l_count = 0;
+        while(lo<=hi){
+            ll mid = lo + (hi-lo)/2;
+
+            if(v[mid] < l) {
+                lessThan_l_count = mid+1;
+                lo = mid+1;
+            }
+            else hi = mid-1;
+        }
+        // cout<<lessThan_l_count<<nl;
+        cout<<n-(greaterThan_r_count+lessThan_l_count)<<" ";
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    // int t,tc=1; cin>>t;
+    // while(t--)
+    SOURAV(1);
+
+return 0;
+}
+
+
+
