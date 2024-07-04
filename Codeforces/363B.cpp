c5fd829213bb1e5a3aef974da94fe0c40860ca84
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-07-04 11:46:51 +0600

    363B

---
+++ Codeforces/363B.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+void SOURAV(int tc){
+    int n,k; cin>>n>>k;
+    vi v(n);
+    For(i,n){
+        cin>>v[i];
+        if(i!=0) v[i] += v[i-1];
+    }
+
+    int mn = v[k-1], ans=1;
+    for(int i=k; i<n; ++i){
+        if(v[i]-v[i-k] < mn){
+            mn = v[i]-v[i-k];
+            ans = i-k+2;
+        }
+    }cout<<ans<<" "<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    // int t,tc=1; cin>>t;
+    // while(t--)
+    SOURAV(1);
+
+return 0;
+}
+
+
+
