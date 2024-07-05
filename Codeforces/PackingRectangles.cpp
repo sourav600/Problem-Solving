52e3840f52cce6c8bac505b59d92c0be2214ffc1
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-07-05 08:55:31 +0600

    packing rectangles (binary search) in c++

---
+++ Codeforces/PackingRectangles.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+__int128 read() {
+    __int128 x = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9') {
+        if (ch == '-') f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9') {
+        x = x * 10 + ch - '0';
+        ch = getchar();
+    }
+    return x * f;
+}
+void print(__int128 x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) print(x / 10);
+    putchar(x % 10 + '0');
+}
+
+void SOURAV(int tc){
+    int w,h,n; cin>>w>>h>>n;
+    ll lo=1, hi=1e18, ans=1;
+    while(lo<=hi){
+        __int128 mid = lo + (hi-lo)/2;
+        __int128 ct = (mid/w)*(mid/h);
+        // print(ct);
+        // cout<<nl;
+        if(ct>=n){
+            ans = mid;
+            hi = mid-1;
+        }
+        else lo = mid+1;
+    }
+    cout<<ans<<nl;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    // int t,tc=1; cin>>t;
+    // while(t--)
+    SOURAV(1);
+
+return 0;
+}
+
+
+
