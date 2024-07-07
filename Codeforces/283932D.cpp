0cb63735a3dc9f7c6ffd04f62d8e45557c62c46d
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-07-07 17:20:06 +0600

    D - Children Holiday

---
+++ Codeforces/283932D.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 2e5 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+int m,n;
+vi t,y,z;
+
+bool isValid(int time){
+    int sum=0;
+    for(int i=0; i<n; ++i){
+        int total_time = t[i]*z[i] + y[i];
+        int ballon = time / total_time;
+        int remain_time = time - (ballon*total_time);
+        ballon *= z[i];
+
+        if(remain_time>=t[i]){
+            ballon += min((remain_time/t[i]), z[i]);
+        }
+        sum += ballon;
+    }
+    return sum >= m;
+}
+
+void SOURAV(int tc){
+     cin>>m>>n;
+    t.resize(n);
+    y.resize(n);
+    z.resize(n);
+    For(i,n){
+        cin>>t[i]>>z[i]>>y[i];
+    }
+    if(m==0) {
+        cout<<0<<nl;
+        For(i,n) cout<<"0 ";
+        return;
+    }
+    int lo=0, hi=1e7;
+    int ans=0;
+    while(lo+1<hi){
+        int mid = lo+(hi-lo)/2;
+
+        if(isValid(mid)){
+            ans = mid;
+            hi = mid;
+        }
+        else lo = mid;
+    }
+    cout<<ans<<nl;
+
+    for(int i=0; i<n; ++i){
+        int total_time = t[i]*z[i] + y[i];
+        int ballon = ans / total_time;
+        int remain_time = ans - (ballon*total_time);
+        ballon *= z[i];
+        if(remain_time>=t[i]){
+            ballon += min((remain_time/t[i]), z[i]);
+        }
+        cout<<min(ballon,m)<<" ";
+        m -= ballon;
+        if(m<0) m=0;
+    }
+
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("../Input.txt", "r", stdin);
+    freopen("../Output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    // int t,tc=1; cin>>t;
+    // while(t--)
+    SOURAV(1);
+
+return 0;
+}
+
+
+
