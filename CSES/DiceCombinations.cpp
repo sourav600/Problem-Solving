8e1e69d8276495eb7b04026c66089183794cdc47
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2024-08-02 22:45:26 +0600

    Dice Combinations CSES

---
+++ CSES/DiceCombinations.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+#define nl '\n'
+#define pi acos(-1)
+#define ll long long
+#define For(i,n) for(ll i=0; i<n; i++)
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define ordered_set tree<pair<int,int>, null_type,less<pair<int,int> >, rb_tree_tag,tree_order_statistics_node_update>
+
+typedef unsigned long long  ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+const ll M = 1e9 + 7;
+const ll N = 1e6 + 10;
+vector<pair<int,int> > movements = { {+1,0},{-1,0},{0,+1},{0,-1} };
+//int dx[]={1,0,-1,0};int dy[]={0,1,0,-1}; //4 Direction
+//int dx[]={1,1,0,-1,-1,-1,0,1};int dy[]={0,1,1,1,0,-1,-1,-1};//8 direction
+//int dx[]={2,1,-1,-2,-2,-1,1,2};int dy[]={1,2,2,1,-1,-2,-2,-1};//Knight Direction
+//int dx[]={2,1,-1,-2,-1,1};int dy[]={0,1,1,0,-1,-1}; //Hexagonal Direction
+ll mod(ll a){ return ((a%M)+M)%M;}
+
+int dp[N];
+ll func(ll val){
+    if(val==0) return 1;
+    if(val<0) return 0;
+    if(dp[val]!=-1) return dp[val];
+
+    ll ans = 0;
+    for(int dice=1; dice<=6; ++dice){
+        ans = mod(ans+func(val-dice));
+    }
+    return dp[val] = ans;
+}
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    // freopen("../Input.txt", "r", stdin);
+    // freopen("../Output.txt", "w", stdout);
+    // #endif
+    memset(dp, -1, sizeof(dp));
+    ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+    int n; cin>>n;
+    cout<<func(n)<<nl;
+
+return 0;
+}
+
+
+
